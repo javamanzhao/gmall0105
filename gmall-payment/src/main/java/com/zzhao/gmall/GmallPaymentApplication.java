2c8777d70c093b68a38912c1d554c75ef44ad24d
Administrator <111@qq.com>
2019-12-11 17:46:54 +0800

    本地提交测试

---
+++ gmall-payment/src/main/java/com/zzhao/gmall/GmallPaymentApplication.java
@@ -0,0 +1,15 @@
+package com.zzhao.gmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.zzhao.gmall.payment.mapper")
+public class GmallPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallPaymentApplication.class, args);
+    }
+
+}
