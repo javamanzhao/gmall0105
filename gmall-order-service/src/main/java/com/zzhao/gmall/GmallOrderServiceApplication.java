7f159fb97fcd05d9be22d6d527960b3fbd2f66eb
Administrator <111@qq.com>
2019-12-11 17:42:05 +0800

    本地提交测试

---
+++ gmall-order-service/src/main/java/com/zzhao/gmall/GmallOrderServiceApplication.java
@@ -0,0 +1,15 @@
+package com.zzhao.gmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.zzhao.gmall.order.mapper")
+public class GmallOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallOrderServiceApplication.class, args);
+    }
+
+}
